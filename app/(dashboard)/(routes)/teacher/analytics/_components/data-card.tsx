6417b2498b425f4527561c6226be7ad85e5b99e3
maskman1543 <103424060+maskman1543@users.noreply.github.com>
2023-11-23 03:12:46 -0400

    Change colors

--- app/(dashboard)/(routes)/teacher/analytics/_components/data-card.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/_components/data-card.tsx
@@ -19,7 +19,7 @@ export const DataCard = ({
 }: DataCardProps) => {
   return (
    <Card>
-    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2 ">
       <CardTitle className="text-sm font-medium">
         {label}
       </CardTitle>

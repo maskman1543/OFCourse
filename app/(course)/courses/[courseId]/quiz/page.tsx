31155afb453a404d493828d0a90bd3ae2c4eca39
charlesdaver <126074952+charlesdaver@users.noreply.github.com>
2023-11-26 23:34:25 -0500

    Quiz front-end

--- app/(course)/courses/[courseId]/quiz/page.tsx
+++ app/(course)/courses/[courseId]/quiz/page.tsx
@@ -1,10 +1,39 @@
-const QuizPage = () => {
+import { auth } from "@clerk/nextjs"
+import { redirect } from "next/navigation";
+
+import { getDashboardCourses } from "@/actions/get-dashboard-courses";
+import { QuizList } from "./_components/quiz-list";
+import { IconBadge } from "@/components/icon-badge";
+import { BookOpenCheck } from "lucide-react";
+
+
+export default async function QuizPage() {
+    const { userId } = auth();
+    if (!userId) {
+        return redirect("/");
+    }    
+        
+    const {
+        completedCourses,
+        coursesInProgress
+      } = await getDashboardCourses(userId);
+
+
     return ( 
 
-    <div>
-        This is a quiz page
-    </div>
-     );
+        <div className="p-6 space-y-4">
+            <div className="flex items-center gap-x-2">
+                <IconBadge icon={BookOpenCheck} />
+                <h2 className="text-xl font-bold ">
+                  Quiz
+                </h2>
+              </div>
+            <QuizList
+                items={[...coursesInProgress, ...completedCourses]}
+            />
+        </div>
+    )
 }
 
-export default QuizPage;
+ 
+
---
+++ components/quiz-card.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+
+interface CourseCardProps {
+    id: string;
+    title: string;
+    imageUrl: string;
+    chaptersLength: number;
+    progress: number | null;
+    category: string;
+  };
+export const QuizCard = ({ id, title }: CourseCardProps) => {
+  const question = "What is the capital of France?";
+  const choices = ["London", "Paris", "Berlin", "Rome"];
+
+  return (
+    <Link href={`/courses/${id}`}>
+      <div className="group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full">
+        <div className="flex flex-col pt-2">
+          <div className="my-3 text-sm md:text-xs">
+            <div className="mb-2">
+              <p className="font-semibold">{question}</p>
+            </div>
+            {choices.map((choice, index) => (
+              <label key={index} className="flex items-center space-x-2">
+                <input
+                  type="radio"
+                  value={choice}
+                  name={`choice_${id}`}
+                  className="form-radio rounded text-sky-700"
+                />
+                <span>{choice}</span>
+              </label>
+            ))}
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};

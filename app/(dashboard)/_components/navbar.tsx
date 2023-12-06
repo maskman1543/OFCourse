4e25fc9f4ea04053f4c2a0455a08d92c4ba5cef1
TheGr8Amon <144041868+TheGr8Amon@users.noreply.github.com>
2023-12-06 21:27:36 +0800

    kaoyon ka ani chris charles?

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -4,7 +4,7 @@ import { MobileSidebar } from "./mobile-sidebar"
 
 export const Navbar = () => {
   return (
-    <div className="p-4 border-b h-full flex items-center bg-blue-50 shadow-sm">
+    <div className="p-4 border-b h-full flex items-center bg-blue-950 shadow-sm">
       <MobileSidebar />
       <NavbarRoutes />
     </div>
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -18,7 +18,9 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        success: "bg-emerald-600 text-white hover:bg-emerald-600/80"
+        success: "bg-emerald-600 text-white hover:bg-emerald-600/80",
+        chanix: 
+          "bg-ghost text-white hover:text-black hover:bg-white/90",
       },
       size: {
         default: "h-10 px-4 py-2",

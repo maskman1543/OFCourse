13ac567f8f5adeb486eca6f070a800c46a37c3f5
charlesdaver <126074952+charlesdaver@users.noreply.github.com>
2023-12-09 01:35:10 -0500

    commented the quiz  student side

--- app/(course)/courses/[courseId]/quiz/_components/quiz-list.tsx
+++ app/(course)/courses/[courseId]/quiz/_components/quiz-list.tsx
@@ -1,43 +1,55 @@
-import { Category, Course } from "@prisma/client";
+// import { Category, Course, Quiz } from "@prisma/client";
+// import { QuizCard } from "@/components/quiz-card";
+// import { useEffect, useState } from "react";
+// import axios from "axios";
 
-import { CourseCard } from "@/components/course-card";
-import { QuizCard } from "@/components/quiz-card";
-import { Button } from "@/components/ui/button";
+// interface quizProp {
+//   id: string;
+//   title: string;
+//   questions: string[]; // Assuming questions is an array of question descriptions
+//   correctAnswer: string; // Assuming correctAnswer is the correct choice
+//   // Other properties...
+// }
 
-type CourseWithProgressWithCategory = Course & {
-  category: Category | null;
-  chapters: { id: string }[];
-  progress: number | null;
-};
+// interface QuizListProps {
+//   quizzes: quizProp [];
+// }
 
-interface CoursesListProps {
-  items: CourseWithProgressWithCategory[];
-}
+// export const QuizList = ({ quizzes }: QuizListProps) => {
+//   const [fetchedQuizzes, setFetchedQuizzes] = useState<Quiz[]>([]);
 
-export const QuizList = ({ items }: CoursesListProps) => {
-    return (
-      <div className="flex flex-col gap-4">
-        <div className="sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4 space-x-1 ">
-          {items.map((item) => (
-            <QuizCard
-              key={item.id}
-              id={item.id}
-              title={item.title}
-              imageUrl={item.imageUrl!}
-              chaptersLength={item.chapters.length}
-              //price={item.price!}
-              progress={item.progress}
-              category={item?.category?.name!}
-            />
-          ))}
-          
-        </div>
-        {items.length === 0 && (
-          <div className="text-center text-sm text-muted-foreground mt-10">
-            No Quiz found
-          </div>
-        )}
-        
-      </div>
-    );
-  };
+//   // Fetch quizzes from the API or any other source
+//   useEffect(() => {
+//     const fetchQuizzes = async () => {
+//       try {
+//         const fetchedQuizzesData = await axios.get<Quiz[]>('api/quizzes');
+//         setFetchedQuizzes(fetchedQuizzesData.data);
+//       } catch (error) {
+//         console.error('Error fetching quizzes:', error);
+//       }
+//     };
+
+//     fetchQuizzes();
+//   }, []);
+
+//   return (
+//     <div className="flex flex-col gap-4">
+//       <div className="sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4 space-x-1 ">
+//         {quizzes.map((quiz) => (
+//           <QuizCard
+//             key={quiz.id}
+//             id={quiz.id}
+//             title={quiz.title}
+//             questions={quiz.questions} // Assuming questions is an array of question descriptions
+//             correctAnswer={quiz.correctAnswer} // Assuming correctAnswer is the correct choice
+//           />
+//         ))}
+//       </div>
+//       {quizzes.length === 0 && (
+//         <div className="text-center text-sm text-muted-foreground mt-10">
+//           No Quiz found
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
--- components/quiz-card.tsx
+++ components/quiz-card.tsx
@@ -1,39 +1,38 @@
-import Link from "next/link";
+// import Link from "next/link";
+// import { Quiz } from "@prisma/client"; // Assuming Quiz is a custom type representing quiz data
 
-interface CourseCardProps {
-    id: string;
-    title: string;
-    imageUrl: string;
-    chaptersLength: number;
-    progress: number | null;
-    category: string;
-  };
-export const QuizCard = ({ id, title }: CourseCardProps) => {
-  const question = "What is the capital of France?";
-  const choices = ["London", "Paris", "Berlin", "Rome"];
 
-  return (
-    <Link href={`/courses/${id}/quiz`}>
-      <div className="group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full">
-        <div className="flex flex-col pt-2">
-          <div className="my-3 text-sm md:text-xs">
-            <div className="mb-2">
-              <p className="font-semibold">{question}</p>
-            </div>
-            {choices.map((choice, index) => (
-              <label key={index} className="flex items-center space-x-3 space-y-2 text-sm">
-                <input
-                  type="radio"
-                  value={choice}
-                  name={`choice_${id}`}
-                  className="form-radio rounded"
-                />
-                <span>{choice}</span>
-              </label>
-            ))}
-          </div>
-        </div>
-      </div>
-    </Link>
-  );
-};
+// interface QuizCardProps {
+//   id: string;
+//   title: string;
+//   questions: string[]; // Array of question descriptions
+//   correctAnswer: string; // Correct answer
+// }
+
+// export const QuizCard = ({ id, title, questions, correctAnswer }: QuizCardProps) => {
+//   // Your component logic to render a quiz card based on the provided data
+//   return (
+//     <Link href={`/courses/${id}/quiz`}>
+//       <div className="group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full">
+//         <div className="flex flex-col pt-2">
+//           <div className="my-3 text-sm md:text-xs">
+//             <div className="mb-2">
+//               <p className="font-semibold">{question || "What is the capital of France?"}</p>
+//             </div>
+//               {choices && choices.map((choice, index) => (
+//                 <label key={index} className="flex items-center space-x-3 space-y-2 text-sm">
+//                   <input
+//                     type="radio"
+//                     value={choice}
+//                     name={`choice_${id}`}
+//                   className="form-radio rounded"
+//                 />
+//                 <span>{choice}</span>
+//               </label>
+//             ))}
+//           </div>
+//         </div>
+//       </div>
+//     </Link>
+//   );
+// };

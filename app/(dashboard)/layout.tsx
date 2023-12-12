97f7f2815d7c28a1d58e1a5e4789e4f6c23efc7d
charlesdaver <126074952+charlesdaver@users.noreply.github.com>
2023-12-12 05:51:42 -0500

    Layout css

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -7,7 +7,7 @@ const DashboardLayout = ({
   children: React.ReactNode;
 }) => {
   return ( 
-    <div className="relative bg-blue-200 min-h-screen">
+    <div className="relative min-h-screen">
       <div className="h-full">
         <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
           <Navbar />
